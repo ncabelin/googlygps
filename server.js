695567a8f0b149fb13582b87f01710916dc8564f
Neptune Michael Cabelin <ncabelin@gmail.com>
2016-07-18 08:40:01 +0000

    added more info

--- server.js
+++ server.js
@@ -6,30 +6,18 @@ var request = require('request');
 var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
 var key = '&key=AIzaSyAay9JvN_HdeAxzEtSKcIgQnrRvB6uuQZ8';
 
-
 app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-
-    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
     next();
 });
 
 
 app.get('/', function(req, res) {
   console.log('write');
-  res.end('Please put zipcode on URL');
+  res.end('Please put zipcode onto URL to get geolocation coordinates : <br>e.g. https://googlygps.herokuapp.com/90016');
 });
 
 
